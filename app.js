e275d78ad2d6b7e01e347ae73a2ce542704d3a5a
iamfareedshaik <iamfareedshaik@gmail.com>
2024-06-02 12:21:46 -0500

    a

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.get("/api/v1/health", (req, res) => {
+  res.send("OK");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
